3ef272318dfd9d0376a17b9c93158cf2ce56df29
Santiago Gutierrez <santi_ago69@live.com>
2021-10-02 12:37:45 -0500

    Le agregan metodos de consulta y los CRUD del inventario

--- README.md
+++ README.md
@@ -2,3 +2,67 @@
 ```bash
 docker-compose run --rm node bash -c "npm install && npm run build"
 ```
+
+# Valores iniciales para existencias
+```js
+[    
+    {
+        "nombre" : "Bomba de infusion",
+        "marca" : "Hospira",
+        "referencia" : "Plum A+",
+        "cantidad" : "5"
+    },
+    {
+        "nombre" : "Regulador vacío adulto",
+        "marca" : "FLOW METER",
+        "referencia" : "VR-C2UD-CMA",
+        "cantidad" : "3"
+    },
+    {
+        "nombre" : "Flujometro",
+        "marca" : "AMVEX",
+        "referencia" : "15 LITROS",
+        "cantidad" : "10"
+    },
+    {
+        "nombre" : "Tensiometro",
+        "marca" : "ALPK2",
+        "referencia" : "NT",
+        "cantidad" : "1"
+    },
+    {
+        "nombre" : "Ventilador",
+        "marca" : "Newport",
+        "referencia" : "E360T",
+        "cantidad" : "9"
+    }
+]
+```
+
+
+# Valores iniciales para proveedores
+```js
+[
+    {
+        "Nombre" : "FLOW METER",
+        "Telefono" : 3322114,
+        "Ciudad" : "Medellin",
+        "Direccion" : "Calle 54 # 2-32",
+        "Email" : "contacto@flowmeter.com"
+    },
+    {
+        "Nombre" : "AT. MEDICAS",
+        "Telefono" : 3654214,
+        "Ciudad" : "Medellin",
+        "Direccion" : "Calle 14 # 21-2",
+        "Email" : "contacto@atmedicas.com"
+    },
+    {
+        "Nombre" : "LINDE COLOMBIA S.A.",
+        "Telefono" : 202154,
+        "Ciudad" : "Medellin",
+        "Direccion" : "Calle 101 # 54-3",
+        "Email" : "contacto@lindesa.com"
+    }
+]
+```
--- api/app.py
+++ api/app.py
@@ -7,14 +7,18 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
 
-@app.route("/movies/<code>", methods=['GET'])
-def get_movie(code):
+### Existencias
+@app.route("/existencias/<code>", methods=['GET'])
+def get_existencia(code):
+    '''
+    Consulta una sola existencia del inventario
+    '''
     con = db.get_connection()
-    dbmov = con.dbmovies
+    dbinv = con.dbinventario
     try:
-        movies = dbmov.movies
+        existencias = dbinv.existencias
         response = app.response_class(
-            response=dumps(movies.find_one({'_id': ObjectId(code)})),
+            response=dumps(existencias.find_one({'_id': ObjectId(code)})),
             status=200,
             mimetype='application/json'
         )
@@ -23,14 +27,17 @@ def get_movie(code):
         con.close()
         print("Connection closed")
 
-@app.route("/movies", methods=['GET'])
-def get_movies():
+@app.route("/existencias", methods=['GET'])
+def get_existencias():
+    '''
+    Consulta todas las existencias del inventario
+    '''
     con = db.get_connection()
-    dbmov = con.dbmovies
+    dbinv = con.dbinventario
     try:
-        movies = dbmov.movies
+        existencias = dbinv.existencias
         response = app.response_class(
-            response=dumps(movies.find()),
+            response=dumps(existencias.find()),
             status=200,
             mimetype='application/json'
         )
@@ -39,27 +46,33 @@ def get_movies():
         con.close()
         print("Connection closed")
 
-@app.route("/movies", methods=['POST'])
-def create():
+@app.route("/existencias", methods=['POST'])
+def create_existencia():
+    '''
+    Inserta un arreglo de existencias en la base de datos
+    '''
     data = request.get_json()
     con = db.get_connection()
-    dbmov = con.dbmovies  
+    dbinv = con.dbinventario
     try:
-        movies = dbmov.movies
-        movies.insert(data)
+        existencias = dbinv.existencias
+        existencias.insert(data)
         return jsonify({"message":"OK"})
     finally:
         con.close()
         print("Connection closed")
 
-@app.route("/movies/<code>", methods=['PUT'])
-def update(code):
+@app.route("/existencias/<code>", methods=['PUT'])
+def update_existencia(code):
+    '''
+    Actualiza la informacion de una sola existencia en el inventario
+    '''
     data = request.get_json()
     con = db.get_connection()
-    dbmov = con.dbmovies
+    dbinv = con.dbinventario
     try:
-        movies = dbmov.movies
-        movies.update(
+        existencias = dbinv.existencias
+        existencias.update(
             {'_id': ObjectId(code)},
             data
         )
@@ -68,13 +81,105 @@ def update(code):
         con.close()
         print("Connection closed")
 
-@app.route("/movies/<code>", methods=['DELETE'])
-def delete(code):
+@app.route("/existencias/<code>", methods=['DELETE'])
+def delete_existencia(code):
+    '''
+    Elimina una sola existencia en el inventario identificada por ID
+    '''
     con = db.get_connection()
-    dbmov = con.dbmovies
+    dbinv = con.dbinventario
     try:
-        movies = dbmov.movies
-        movies.delete_one({'_id': ObjectId(code)})
+        existencias = dbinv.existencias
+        existencias.delete_one({'_id': ObjectId(code)})
+        return jsonify({"message":"OK"})
+    finally:
+        con.close()
+        print("Connection closed")
+
+### Proveedores
+@app.route("/existencias/<code>", methods=['GET'])
+def get_proveedor():
+    '''
+    Consulta un solo proveedor por ID
+    '''
+    con = db.get_connection()
+    dbinv = con.dbinventario
+    try:
+        proveedores = dbinv.proveedores
+        proveedores = app.response_class(
+            response=dumps(proveedores.find_one({'_id': ObjectId(code)})),
+            status=200,
+            mimetype='application/json'
+        )
+        return response
+    finally:
+        con.close()
+        print("Connection closed")
+
+@app.route("/proveedores", methods=['GET'])
+def get_proveedores():
+    '''
+    Consulta todos los proveedores del inventario
+    '''
+    con = db.get_connection()
+    dbinv = con.dbinventario
+    try:
+        proveedores = dbinv.proveedores
+        response = app.response_class(
+            response=dumps(proveedores.find()),
+            status=200,
+            mimetype='application/json'
+        )
+        return response
+    finally:
+        con.close()
+        print("Connection closed")
+
+@app.route("/proveedores", methods=['POST'])
+def create_proveedores():
+    '''
+    Inserta un arreglo de proveedores en la base de datos
+    '''
+    data = request.get_json()
+    con = db.get_connection()
+    dbinv = con.dbinventario
+    try:
+        proveedores = dbinv.proveedores
+        proveedores.insert(data)
+        return jsonify({"message":"OK"})
+    finally:
+        con.close()
+        print("Connection closed")
+
+@app.route("/proveedores/<code>", methods=['PUT'])
+def update_proveedor(code):
+    '''
+    Actualiza la informacion de un solo proveedor en el inventario
+    '''
+    data = request.get_json()
+    con = db.get_connection()
+    dbinv = con.dbinventario
+    try:
+        proveedores = dbinv.proveedores
+        proveedores.update(
+            {'_id': ObjectId(code)},
+            data
+        )
+        return jsonify({"message":"OK"})
+    finally:
+        con.close()
+        print("Connection closed")
+
+@app.route("/proveedores/<code>", methods=['DELETE'])
+def delete_proveedor(code):
+    '''
+    Elimina un solo proveedor en el inventario identificada por ID
+    '''
+    con = db.get_connection()
+    dbinv = con.dbinventario
+    try:
+        proveedores = dbinv.proveedores
+        proveedores.delete_one({'_id': ObjectId(code)})
         return jsonify({"message":"OK"})
     finally:
         con.close()
